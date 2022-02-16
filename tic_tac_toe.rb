f0a04641fa13f2966f985c282c6e2238c00d3a56
Eric Romano <ecr1980@gmail.com>
2022-02-16 11:54:29 -0500

    Now includes game logic. Goes between both players giving them the choice of board to play. Displays an error message if player input is not a board piece. Checks for multiple version of valid input. For instance, A1, a1, A 1, and a 1 are all valid inputs for the top left square. Tells the player if the square they have chosen has already been played. Once a player selects a valid square, their marker is placed and kept through each round. If the player fails to choose a valid option during their turn and gets one of the above error messages, it is stil their turn and they may try again.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -43,4 +43,91 @@ def gameboard()
   puts "C  #{$c_1.display()} | #{$c_2.display()} |#{$c_3.display()} "
 end
 
-gameboard()
+def play(x)
+  if x == true
+    player = 1
+  else
+    player = 2
+  end
+  puts "Player #{player}, it is your turn! Please enter your choice."
+  puts "Example, enter A1 for the top left square, or C3 for the bottom right."
+  gameboard()
+  choice = gets
+  choice = choice.downcase.chomp
+  choice = choice.delete(' ')
+   if is_valid_choice(choice)
+    select(choice, player)
+   else
+    puts "I did not understand that, try again"
+    puts "your entered string shows as '#{choice}'."
+   end
+  
+end
+
+def select(str, player)
+  case str
+  when "a1"
+    $a_1.play(player)
+  when "a2"
+    $a_2.play(player)
+  when "a3"
+    $a_3.play(player)
+  when "b1"
+    $b_1.play(player)
+  when "b2"
+    $b_2.play(player)
+  when "b3"
+    $b_3.play(player)
+  when "c1"
+    $c_1.play(player)
+  when "c2"
+    $c_2.play(player)
+  when "c3"
+    $c_3.play(player)
+  else
+    puts "I'm not sure how we got this error."
+  end
+end
+
+def is_valid_choice(str)
+  case str
+  when "a1"
+    return true
+  when "a2"
+    return true
+  when "a3"
+    return true
+  when "b1"
+    return true
+  when "b2"
+    return true
+  when "b3"
+    return true
+  when "c1"
+    return true
+  when "c2"
+    return true
+  when "c3"
+    return true
+  else
+    return false
+  end
+end
+
+
+
+
+game = true
+turn = true
+
+while (game == true)
+  if (turn == true)
+    if play(true)
+    turn = false
+    end
+  elsif
+    if play(false)
+    turn = true
+    end
+  end
+end
